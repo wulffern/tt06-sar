4915988250f47bb9a8dd47b5cacdaabf7bfc5cca
Carsten Wulff <carsten@wulff.no>
2024-04-18 12:19:47 +0200

    Updated verilog model

--- src/project.v
+++ src/project.v
@@ -1,4 +1,5 @@
 `default_nettype none
+`timescale 1 ns / 1 ps
 
 
 module tt_um_TT06_SAR_wulffern (
@@ -10,8 +11,8 @@ module tt_um_TT06_SAR_wulffern (
                                 output wire [7:0] uio_out,
                                 output wire [7:0] uio_oe,
 `ifdef ANA_TYPE_REAL
-                                input real ua_0,
-                                input real ua_1,
+                                input real        ua_0,
+                                input real        ua_1,
 `else
                                 inout wire [7:0]  ua, // analog pins
 `endif
@@ -27,70 +28,136 @@ module tt_um_TT06_SAR_wulffern (
    logic [1:0]                                    state;
    logic [1:0]                                    next_state;
    logic [7:0]                                    dout;
-
+   logic [7:0]                                    sampled_dout;
+   logic                                          done;
+   logic                                          tie_l  =0;
+   logic                                          tie_h = 1;
 
    always_comb begin
       case (state)
-        OFF: next_state = SAMPLE;
-        SAMPLE: next_state = CONVERT;
-        CONVERT: next_state = SAMPLE;
+        OFF: begin
+           next_state = SAMPLE;
+        end
+        SAMPLE: begin
+           next_state = CONVERT;
+        end
+        CONVERT: begin
+           next_state = DONE;
+        end
+        DONE: begin
+           next_state = SAMPLE;
+        end
+
         default: next_state = OFF;
       endcase // case (state)
    end
 
 `ifdef ANA_TYPE_REAL
    real tmp = 0;
+   real smpl = 0;
+   real lsb = 1.0/64.0;
 `else
-   logic          tmp;
+   logic tmp;
 
 `endif
 
-   assign uo_out = dout;
+   assign uo_out = sampled_dout;
+   assign uio_out[0] = done;
+
+   assign uio_out[1] = tie_l;
+   assign uio_out[2] = tie_l;
+   assign uio_out[3] = tie_l;
+   assign uio_out[4] = tie_l;
+   assign uio_out[5] = tie_l;
+   assign uio_out[6] = tie_l;
+   assign uio_out[7] = tie_l;
+   assign uio_oe[0] = tie_h;
+   assign uio_oe[1] = tie_l;
+   assign uio_oe[2] = tie_l;
+   assign uio_oe[3] = tie_l;
+   assign uio_oe[4] = tie_l;
+   assign uio_oe[5] = tie_l;
+   assign uio_oe[6] = tie_l;
+   assign uio_oe[7] = tie_l;
 
 
+   //always #5 iclk = !iclk;
+//& ~done & ui_in[0];
 
 
-   always_ff @(posedge clk) begin
+   always_ff @(posedge clk or negedge clk) begin
 
-      if(~ui_in[1]) begin
-        state <= OFF;
+      if(~ui_in[0]) begin
+         state <= OFF;
          tmp = 0;
-         end
-
+         dout = 0;
+      end
       else begin
+         if(OFF) begin
 
-`ifdef ANA_TYPE_REAL
-         if(SAMPLE)
-           tmp = ua_1 - ua_0;
-         else if(CONVERT) begin
+         end
+         else if(clk == 1) begin
+            state = SAMPLE;
+         end// !`ifdef ANA_TYPE_REAL
+         else if(clk == 0) begin
+            state = CONVERT;
+ `ifdef ANA_TYPE_REAL
+            smpl = ua_1 - ua_0;
+            tmp = smpl;
+
             for(int i=7;i>=0;i--) begin
                if(tmp >= 0) begin
-                  tmp = tmp*2 - 1;
+                  tmp = tmp - lsb*2**(i-1);
                   if(i==7)
-                    uo_out[i] <= 0;
+                    dout[i] <= 0;
                   else
-                    uo_out[i] <= 1;
+                    dout[i] <= 1;
                end
                else begin
-                  tmp = tmp*2 + 1;
+                  tmp = tmp + lsb*2**(i-1);
                   if(i==7)
                     dout[i] = 1;
                   else
                     dout[i] = 0;
                end
             end
-         end
-`else
-         if(CONVERT) begin
-            if(tmp == 0)
-              dout[7] = 1;
-            else
-              dout[7] = 0;
-        end
-`endif
-         state = next_state;
-      end
+ `else
+            if(tmp == 0) begin
+              dout[7] <= 1;
+               tmp <= 1;
 
+            end
+            else begin
+              dout[7] <= 0;
+               tmp  = 0;
+            end
+ `endif
+
+         end
+
+         state = next_state;
+      end // else: !if(~ui_in[0])
+
+
+
+
+
+   end // always_ff @ (posedge clk)
+
+   always @(posedge done) begin
+      state = DONE;
+      sampled_dout = dout;
+
+
+   end
+
+   always @(state) begin
+      if(state == OFF)
+        #2 done = 0;
+      else if(state == SAMPLE)
+        #1.6 done = 0;
+      else if(state == CONVERT)
+        #115 done = 1;
    end
 
 endmodule
--- src/tb_ana.v
+++ src/tb_ana.v
@@ -1,23 +1,30 @@
 
 `timescale 1 ns / 1 ps
-
 module test;
 
   logic rst_n = 0;
 
   logic clk = 0;
-  always #5 clk = !clk;
+  always #125 clk = !clk;
 
    logic VGND;
    logic VPWR;
 
-    wire [7:0] ui_in;
-   wire [7:0] uo_out;
-   wire [7:0] uio_in;
-   wire [7:0] uio_out;
-   wire [7:0] uio_oe;
-   real        ua_0;
-   real        ua_1;
+   logic [7:0] ui_in;
+   logic [7:0] uo_out;
+   logic [7:0] uio_in;
+   logic [7:0] uio_out;
+   logic [7:0] uio_oe;
+
+`ifdef ANA_TYPE_REAL
+   real      ua_0  = 0;
+   real      ua_1 = 0;
+
+`else
+   tri [7:0] ua;
+   logic     uain = 0;
+   assign ua = uain;
+`endif
    wire       ena;
 
 
@@ -30,22 +37,47 @@ module test;
                                 .uio_in(uio_in),
                                 .uio_out(uio_out),
                                 .uio_oe(uio_oe),
+`ifdef ANA_TYPE_REAL
                                 .ua_0(ua_0),
                                 .ua_1(ua_1),
+`else
+                                .ua(ua),
+`endif
                                 .ena(ena),
                                 .clk(clk),
                                 .rst_n(rst_n)
                                 );
+
+
+`ifdef ANA_TYPE_REAL
+
+
+   always #100 begin
+      ua_0 = $sin(2*3.14*1/7750*$time);
+      ua_1 = -$sin(2*3.14*1/7750*$time);
+   end
+
+`endif
+
+
 
   initial
     begin
        $dumpfile("test.vcd");
        $dumpvars(0,test);
 
+       ui_in = 0;
+
+
        #10 rst_n = 0;
        #10 rst_n = 1;
 
-       #2600 $stop;
+       #10 VGND = 0;
+       #1 VPWR = 1;
+
+       #500 ui_in[0] = 1;
+
+       #5000 $stop;
 
     end
 endmodule // test
